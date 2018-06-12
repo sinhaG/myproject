b18daf7439a4d5710f3d0d8c33984a9acfe6a24e
sinhaG <grv.sinha@gmail.com>
2018-06-12 11:47:15 +0530

    Week5 Complete

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
+
 # Create your models here.
 
 class Board(models.Model): 
@@ -9,13 +11,21 @@ class Board(models.Model):
 
     def __str__(self):
         return self.name
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
 
 class Topic(models.Model): 
     subject = models.CharField(max_length = 255)
     last_updated = models.DateTimeField(auto_now_add=True)
     board = models.ForeignKey(Board,on_delete = models.CASCADE ,related_name='topics')
     starter = models.ForeignKey(User,on_delete = models.CASCADE, related_name='topics')
-    temp= models.CharField(max_length=100)
+    #temp= models.CharField(max_length=100)
+    views = models.PositiveIntegerField(default=0)
+    def __str__(self):
+        return self.subject
 class Post(models.Model): 
     message = models.TextField(max_length=4000)
     topic = models.ForeignKey(Topic,on_delete = models.CASCADE, related_name='posts')
@@ -25,10 +35,14 @@ class Post(models.Model):
     updated_by = models.ForeignKey(User, on_delete = models.CASCADE , null=True, related_name='+')
     temp= models.CharField(max_length=100)
 
-class Boarding(models.Model): 
-    name = models.CharField(max_length =30, unique=True)
-    description = models.CharField(max_length=100)
-    temp= models.CharField(max_length=100)
-
     def __str__(self):
-        return self.name
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
+
+# class Boarding(models.Model): 
+#     name = models.CharField(max_length =30, unique=True)
+#     description = models.CharField(max_length=100)
+#     temp= models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name
